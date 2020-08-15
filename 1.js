bcc5a8c0069b955836129885ba66d3a220a518c8
dangphu <nguyendangphu1234567890@gmail.com>
2020-08-15 22:11:54 +0700

    clone xong

--- 1.js
+++ 1.js
@@ -10,7 +10,11 @@
  	  	return false ; 
  	  
  	 })
- 	
+ 	$(".lh").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#lienhe').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
 	$(".cm").click(function(){
  	  	$('body,html').animate({scrollTop:$('#phanhoi').offset().top},700);
  	  	return false ; 
