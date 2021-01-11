7a95983ee460575c144b78d35774ac6248b72cbe
dangphu <nguyendangphu1234567890@gmail.com>
2021-01-11 20:41:43 +0700

    web 1 web 2

---
+++ Web2/1.js
@@ -0,0 +1,39 @@
+﻿$(function(){
+	$('.a').click(function(){
+    	$('html,body').animate({scrollTop:$('.top2').offset().top});
+    	return false;
+    })
+   
+    $('.b').click(function(){
+    	$('html,body').animate({scrollTop:$('.top3').offset().top});
+    	return false;
+    })
+
+    $('.c').click(function(){
+    	$('html,body').animate({scrollTop:$('.top4').offset().top});
+    	return false;
+    })
+
+ 	var docao=$(window).height()
+ 	$('.top1').css({'height':docao})
+ 	
+ 	$(window).resize(function(){
+ 		console.log(docao)
+ 	$('.top1').css({'height':docao})
+
+ 	});
+
+
+
+ 	$('.nutmenu').click(function(){
+ 		$('.menuphai').addClass('ra');
+ 		return false;
+
+ 	})
+ 	$('.tat').click(function(){
+ 		$('.menuphai').removeClass('ra');
+ 		return false;
+
+ 	})
+	 
+})   
